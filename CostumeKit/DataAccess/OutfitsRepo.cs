2e05d829011ae988b04b6e582b321fd10bccc5fd
jthielman <john.thielman@gmail.com>
2020-07-11 13:29:46 -0500

    api works

--- CostumeKit/DataAccess/OutfitsRepo.cs
+++ CostumeKit/DataAccess/OutfitsRepo.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CostumeKit.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +11,26 @@ namespace CostumeKit.DataAccess
 {
     public class OutfitsRepo
     {
+        string connectionString;
+        public OutfitsRepo(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("CostumeKit");
+        }
+
+        public IEnumerable<Outfit> GetUserOutfits(int userId)
+        {
+            var sql = @"
+                        select *
+                        from [Outfits]
+                        where UserId = @UserId;
+                        ";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { UserId = userId };
+                var result = db.Query<Outfit>(sql, parameters);
+                return result;
+            }
+        }
     }
 }
