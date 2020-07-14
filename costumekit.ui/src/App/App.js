7431767e2995f4d5b0fdf763ce89777a23296696
jthielman <john.thielman@gmail.com>
2020-07-13 19:18:31 -0500

    ui setup done

---
+++ costumekit.ui/src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-success">hey</button>
+      <p><i class="fab fa-accessible-icon"></i></p>
+      <h2>INSIDE APP COMPONENT</h2>
+      <button className="btn btn-info"><i className="fas fa-rocket"></i></button>
+    </div>
+  );
+}
+
+export default App;
