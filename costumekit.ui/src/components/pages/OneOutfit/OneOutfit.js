49470cb396cd10243f26847d3c141d76d36aee6e
jthielman <john.thielman@gmail.com>
2020-07-20 18:53:46 -0500

    made outfit page

---
+++ costumekit.ui/src/components/pages/OneOutfit/OneOutfit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './OneOutfit.scss';
+
+class OneOutfit extends React.Component {
+  render() {
+    return <h1>Single Outfit</h1>;
+  }
+}
+
+export default OneOutfit;
