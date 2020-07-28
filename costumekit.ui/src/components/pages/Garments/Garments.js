9f391dbf2f7dcb00e870b4ed2a2ac6862961ca2f
jthielman <john.thielman@gmail.com>
2020-07-28 18:55:17 -0500

    data reaches component

--- costumekit.ui/src/components/pages/Garments/Garments.js
+++ costumekit.ui/src/components/pages/Garments/Garments.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import garmentData from '../../../helpers/data/garmentData';
 
 class Garments extends React.Component {
+  state = {
+    garments: [],
+  }
+
+  getAllGarments = () => {
+    garmentData.getAllGarments()
+      .then((garments) => this.setState({ garments }))
+      .catch((err) => console.error('error in getAllGarments', err));
+  }
+
+  componentDidMount() {
+    this.getAllGarments();
+  }
+
   render() {
     return (
       <h1>Garments Page</h1>
