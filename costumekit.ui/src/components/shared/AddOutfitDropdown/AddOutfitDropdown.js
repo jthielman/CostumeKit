db99e37330a2f2221d2387d880d5818df93d69a6
jthielman <john.thielman@gmail.com>
2020-07-27 21:28:24 -0500

    form validation in place

--- costumekit.ui/src/components/shared/AddOutfitDropdown/AddOutfitDropdown.js
+++ costumekit.ui/src/components/shared/AddOutfitDropdown/AddOutfitDropdown.js
@@ -11,7 +11,12 @@ class AddOutfitDropdown extends React.Component {
     settings: [],
     newOutfitName: '',
     newOutfitDescription: '',
-    newOutfitSettingId: 1,
+    newOutfitSettingId: 0,
+    errors: {
+      name: '',
+      description: '',
+      setting: '',
+    },
   }
 
   getSettings = () => {
@@ -29,39 +34,65 @@ class AddOutfitDropdown extends React.Component {
     document.getElementById('new-outfit-form').classList.toggle('show');
   }
 
-  nameChange = (e) => {
-    e.preventDefault();
-    this.setState({ newOutfitName: e.target.value });
+  handleChange = (event) => {
+    event.preventDefault();
+    const { id, value } = event.target;
+    const { errors } = this.state;
+
+    switch (id) {
+      case 'newOutfitName':
+        errors.name = value.length > 50 || value.length === 0
+          ? 'Name must be between 1 and 50 characters'
+          : '';
+        break;
+      case 'newOutfitDescription':
+        errors.description = value.length > 200
+          ? 'Description limited to 200 characters'
+          : '';
+        break;
+      case 'newOutfitSettingId':
+        errors.setting = (value === '')
+          ? 'Please choose a setting'
+          : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [id]: value });
   }
 
-  descriptionChange = (e) => {
-    e.preventDefault();
-    this.setState({ newOutfitDescription: e.target.value });
-  }
-
-  settingChange= (e) => {
-    e.preventDefault();
-    this.setState({ newOutfitSettingId: parseInt(e.target.value, 10) });
+  validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+      // if we have an error string set valid to false
+      (val) => {
+        val.length > 0 && (valid = false);
+      },
+    );
+    return valid;
   }
 
   addOutfit = (e) => {
     e.preventDefault();
     const {
-      newOutfitName, newOutfitDescription, userId, newOutfitSettingId,
+      errors, newOutfitName, newOutfitDescription, userId, newOutfitSettingId,
     } = this.state;
-    const outfitToAdd = {
-      name: newOutfitName,
-      description: newOutfitDescription,
-      userId,
-      settingId: newOutfitSettingId,
-    };
-    outfitData.addOutfit(outfitToAdd)
-      .then((newOutfit) => this.props.history.push(`/outfit/${newOutfit.id}`))
-      .catch((err) => console.error('error in addOutfit', err));
+    if (this.validateForm(errors) && newOutfitSettingId > 0) {
+      const outfitToAdd = {
+        name: newOutfitName,
+        description: newOutfitDescription,
+        userId,
+        settingId: newOutfitSettingId,
+      };
+      outfitData.addOutfit(outfitToAdd)
+        .then((newOutfit) => this.props.history.push(`/outfit/${newOutfit.id}`))
+        .catch((err) => console.error('error in addOutfit', err));
+    }
   }
 
   render() {
-    const { settings } = this.state;
+    const { settings, errors } = this.state;
     return (
       <div className="AddOutfitDropdown dropdown">
           <button onClick={this.openDropdown} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -69,20 +100,24 @@ class AddOutfitDropdown extends React.Component {
           </button>
           <form className="dropdown-menu p-4" id="new-outfit-form" aria-labelledby="dropdownMenuButton">
             <div className="form-group">
-              <label htmlFor="outfitName">Name your outfit</label>
-              <input type="text" className="form-control" id="outfitName" placeholder="Aragorn" onChange={this.nameChange}/>
+              <label htmlFor="newOutfitName">Name your outfit</label>
+              <input type="text" required className="form-control" id="newOutfitName" placeholder="Aragorn" onChange={this.handleChange}/>
+              <span className="error">{errors.name.length > 0 && errors.name}</span>
             </div>
             <div className="form-group">
-              <label htmlFor="outfitDescription">Tell about your outfit</label>
-              <textarea className="form-control" id="outfitDescription" rows="3" placeholder="Isildur's heir, Elessar, the Elfstone, Telcontar, Thorongil..." onChange={this.descriptionChange} />
+              <label htmlFor="newOutfitDescription">Tell about your outfit</label>
+              <textarea required className="form-control" id="newOutfitDescription" rows="3" placeholder="Isildur's heir, Elessar, the Elfstone, Telcontar..." onChange={this.handleChange} />
+              <span className="error">{errors.description.length > 0 && errors.description}</span>
             </div>
             <div className="form-group">
-              <label htmlFor="settingDropdown">Pick a setting</label>
-              <select className="form-control" id="settingDropdown" onChange={this.settingChange}>
+              <label htmlFor="newOutfitSettingId">Pick a setting</label>
+              <select required className="form-control" id="newOutfitSettingId" onChange={this.handleChange}>
+                <option key="0" value={null}></option>
                 {settings.map((setting) => <option key={setting.id} value={setting.id}>{setting.name}</option>)}
               </select>
+              <span className="error">{errors.setting.length > 0 && errors.setting}</span>
             </div>
-            <button onClick={this.addOutfit} type="submit" className="btn btn-primary">Add outfit</button>
+            <button onClick={this.addOutfit} type="button" className="btn btn-primary">Add outfit</button>
           </form>
         </div>
     );
