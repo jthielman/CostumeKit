d5be127376d70170380f034886c82a93d199bb77
jthielman <john.thielman@gmail.com>
2020-07-28 20:31:38 -0500

    added link to garment page in navbar

--- costumekit.ui/src/components/shared/MyNavbar/MyNavbar.js
+++ costumekit.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './MyNavbar.scss';
 
-const MyNavbar = () => <nav className="MyNavbar navbar"><h1>Costume Kit</h1></nav>;
+const MyNavbar = () => (
+  <div className="MyNavbar">
+    <nav className="navbar navbar-expand-sm navbar-light">
+      <Link to="/" className="brand"><h1>Costume Kit</h1></Link>
+      <div className="collapse navbar-collapse show">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/garments">Garments</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
 
 export default MyNavbar;
