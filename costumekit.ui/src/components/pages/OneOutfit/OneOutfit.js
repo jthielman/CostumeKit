e6c577b7a322f9eefb4b8c5fe9da0e7d9577d7f1
jthielman <john.thielman@gmail.com>
2020-08-01 10:58:21 -0500

    edit buttons

--- costumekit.ui/src/components/pages/OneOutfit/OneOutfit.js
+++ costumekit.ui/src/components/pages/OneOutfit/OneOutfit.js
@@ -36,7 +36,17 @@ class OneOutfit extends React.Component {
 
   editButtonClick = (e) => {
     e.preventDefault();
-    this.setState({ editMode: true });
+    e.persist();
+    console.log(e);
+    const { id } = e.currentTarget;
+    this.setState({ [id]: true });
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    const { id, value } = e.target;
+    const { outfit } = this.state;
+    this.setState({ outfit, [id]: value });
   }
 
   render() {
@@ -54,34 +64,39 @@ class OneOutfit extends React.Component {
           <div className="col-4">
             {nameEditMode
               ? <div className="row input-group">
-                  <input id="name" className="form-control form-control-lg" type="text" value={outfit.name} />
+                  <input id="name" className="form-control form-control-lg" type="text" value={outfit.name} onChange={this.handleChange} />
                   <div className="input-group-append">
                     <button type="button" className="btn btn-outline-secondary"><i className="fas fa-check"></i></button>
                   </div>
                 </div>
               : <div id="Name" className="row d-flex justify-content-between align-items-baseline">
                   <h2>{outfit ? outfit.name : ''}</h2>
-                  <button type="button" className="btn text-muted" id="editName" onClick={this.editButtonClick}><i className="fas fa-pen-fancy"></i></button>
+                  <button type="button" className="btn text-muted" id="nameEditMode" onClick={this.editButtonClick}><i className="fas fa-pen-fancy"></i></button>
                 </div>
             }
             {settingEditMode
               ? <div className="row input-group">
-                  <input id="setting" className="form-control" type="text" />
+                  <input id="setting" className="form-control" type="text" value={outfit.settingName} onChange={this.handleChange} />
                   <div className="input-group-append">
                     <button type="button" className="btn btn-outline-secondary"><i className="fas fa-check"></i></button>
                   </div>
                 </div>
               : <div className="row d-flex justify-content-between align-items-baseline">
                   <h4 className="text-muted">{outfit.settingName}</h4>
-                  <button type="button" className="btn text-muted" id="editSetting" onClick={this.editButtonClick}><i className="fas fa-pen-fancy"></i></button>
+                  <button type="button" className="btn text-muted" id="settingEditMode" onClick={this.editButtonClick}><i className="fas fa-pen-fancy"></i></button>
                 </div>
             }
           </div>
           {descriptionEditMode
-            ? <textarea id="description" value={outfit.description} />
+            ? <div className="col-6 input-group">
+                <textarea id="description" className="form-control" value={outfit.description} onChange={this.handleChange} />
+                <div className="input-group-append">
+                  <button type="button" className="btn btn-outline-secondary"><i className="fas fa-check"></i></button>
+                </div>
+              </div>
             : <div className="row d-flex justify-content-between align-items-baseline">
                 <p className="outfit-description col">{outfit.description}</p>
-                <button type="button" className="btn text-muted" id="editSetting" onClick={this.editButtonClick}><i className="fas fa-pen-fancy"></i></button>
+                <button type="button" className="btn text-muted" id="descriptionEditMode" onClick={this.editButtonClick}><i className="fas fa-pen-fancy"></i></button>
               </div>
           }
         </div>
