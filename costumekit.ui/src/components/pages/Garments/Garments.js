75de66f6f51f9edcfb2bd7ec9290d17fc84af16a
jthielman <john.thielman@gmail.com>
2020-08-01 14:49:57 -0500

    first steps

--- costumekit.ui/src/components/pages/Garments/Garments.js
+++ costumekit.ui/src/components/pages/Garments/Garments.js
@@ -25,7 +25,7 @@ class Garments extends React.Component {
     const { garments } = this.state;
     return (
       <div className="Garments container">
-        <h1>Garments Page</h1>
+        <h2>Garments Page</h2>
         <div className="row">
           {garments.map((garment) => <GarmentCard garment={garment} key={garment.id} />)}
         </div>
--- costumekit.ui/src/components/shared/GarmentCard/GarmentCard.js
+++ costumekit.ui/src/components/shared/GarmentCard/GarmentCard.js
@@ -11,6 +11,16 @@ class GarmentCard extends React.Component {
         <div className="card-body">
           <h5 className="card-title">{garment.name}</h5>
           <p className="card-text">{garment.description}</p>
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
           {/* TODO: decide if there needs to be a link here to a garment page
           <Link to={`./garment/${garment.id}`} className="card-link">Look at this garment</Link>
           <a href="#" className="card-link">Another link</a> */}
