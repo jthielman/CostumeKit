6288ed75d996ab2d01577f425015e213bd0a5968
jthielman <john.thielman@gmail.com>
2020-08-03 18:22:03 -0500

    dropdowns on garment cards with outfit names

--- costumekit.ui/src/components/shared/GarmentCard/GarmentCard.js
+++ costumekit.ui/src/components/shared/GarmentCard/GarmentCard.js
@@ -1,24 +1,47 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
+import outfitData from '../../../helpers/data/outfitData';
+
 import './GarmentCard.scss';
 
 class GarmentCard extends React.Component {
+  state = {
+    outfits: [],
+    userId: 1,
+  }
+
+  getOutfits = () => {
+    const { userId } = this.state;
+    outfitData.getUserOutfits(userId)
+      .then((outfits) => this.setState({ outfits }))
+      .catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getOutfits();
+  }
+
+  openDropdown = (e) => {
+    e.preventDefault();
+    const { garment } = this.props;
+    document.getElementById(`add-garment-dropdown${garment.id}`).classList.toggle('show');
+  }
+
   render() {
     const { garment } = this.props;
+    const { outfits } = this.state;
     return (
       <div className="card col-md-3" key={garment.id}>
         <div className="card-body">
           <h5 className="card-title">{garment.name}</h5>
           <p className="card-text">{garment.description}</p>
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown button
+          <div className="dropdown show">
+            <button onClick={this.openDropdown} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+              Add to outfit
             </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
+            <div id={`add-garment-dropdown${garment.id}`} className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              {outfits.map((outfit) => <button className="dropdown-item" type="button" key={outfit.id}>{outfit.name}</button>)}
             </div>
           </div>
           {/* TODO: decide if there needs to be a link here to a garment page
