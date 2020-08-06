1965ed8c3c8fa130ca74debd779919157a93943d
jthielman <john.thielman@gmail.com>
2020-08-06 18:23:42 -0500

    favicon

--- costumekit.ui/src/components/shared/OutfitCard/OutfitCard.js
+++ costumekit.ui/src/components/shared/OutfitCard/OutfitCard.js
@@ -12,7 +12,7 @@ class OutfitCard extends React.Component {
           <h5 className="card-title">{outfit.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{outfit.settingName}</h6>
           <p className="card-text">{outfit.description}</p>
-          <Link to={`./outfit/${outfit.id}`} className="card-link link">Look at this outfit</Link>
+          <Link to={`./outfit/${outfit.id}`} className="btn btn-success">See this outfit</Link>
           {/* TODO: decide if there needs to be another link here
           <a href="#" className="card-link">Another link</a> */}
         </div>
